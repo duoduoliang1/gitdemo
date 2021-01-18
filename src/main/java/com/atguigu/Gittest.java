048cb2248509978272ecca8a1cfb801414225236
gitBO <1312091083@qq.com>
2021-01-18 23:01:42 +0800

    更新a

--- src/main/java/com/atguigu/Gittest.java
+++ src/main/java/com/atguigu/Gittest.java
@@ -4,5 +4,6 @@ public class Gittest {
     public static void main(String[] args) {
         System.out.println("11111");
         System.out.println("22222");
+        System.out.println("a");
     }
 }
