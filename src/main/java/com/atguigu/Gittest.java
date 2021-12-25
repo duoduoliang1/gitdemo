986f53f8a0a78ed1292f8a9017c5bc09765bfb83
duoduoliang1 <77619530+duoduoliang1@users.noreply.github.com>
2021-12-25 16:07:48 +0800

    Update Gittest.java

--- src/main/java/com/atguigu/Gittest.java
+++ src/main/java/com/atguigu/Gittest.java
@@ -4,11 +4,5 @@ public class Gittest {
     public static void main(String[] args) {
         System.out.println("11111");
         System.out.println("22222");
-        System.out.println("a");
-        System.out.println("b");
-        System.out.println("合并");
-        System.out.println("dev123");
-        System.out.println("dev修改");
-        System.out.println("pull");
     } 
 }
