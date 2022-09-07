caa9fe495f9df46301787e895b8ad0f41dfe42cf
duoduoliang1 <77619530+duoduoliang1@users.noreply.github.com>
2022-09-07 17:58:05 +0800

    Update Gittest.java
    123

--- src/main/java/com/atguigu/Gittest.java
+++ src/main/java/com/atguigu/Gittest.java
@@ -7,6 +7,6 @@ public class Gittest {
         System.out.println("caozuo");
         System.out.println("aaa");
         System.out.println("aaa123");
-        System.out.println("bbb111");
+       
     }
 }
